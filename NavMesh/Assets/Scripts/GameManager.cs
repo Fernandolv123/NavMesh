a0d6b1fcf7af4e8cdc65e47b6c7dd506ed37476c
fernandolv123 <ferlorenzovazquez@gmail.com>
2024-04-25 17:53:23 +0200

    problema de la camara arreglado

--- NavMesh/Assets/Scripts/GameManager.cs
+++ NavMesh/Assets/Scripts/GameManager.cs
@@ -15,26 +15,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //esto esta acumulando velocidad, hay que replaantearse la posicion del clamp
         float moveAmount = 100f;
         float edgeSize = 10f;
         if(Input.mousePosition.x > Screen.width - edgeSize){
-            //127 y -7
             cameraFollowPosition.x -= moveAmount * Time.deltaTime;
-            cameraFollow.transform.position = CameraClamped(cameraFollowPosition);
         }
         if (Input.mousePosition.x < edgeSize){
             cameraFollowPosition.x += moveAmount * Time.deltaTime;
-            cameraFollow.transform.position = CameraClamped(cameraFollowPosition);
         }
         if (Input.mousePosition.y > Screen.height - edgeSize){
-            //clamp 23 y -23
             cameraFollowPosition.z -= moveAmount * Time.deltaTime;
-            cameraFollow.transform.position = CameraClamped(cameraFollowPosition);
         }
         if (Input.mousePosition.y < edgeSize){
             cameraFollowPosition.z += moveAmount * Time.deltaTime;
-            cameraFollow.transform.position = CameraClamped(cameraFollowPosition);
         }
+        cameraFollowPosition = new Vector3(Mathf.Clamp(cameraFollowPosition.x,-7,127),cameraFollowPosition.y,Mathf.Clamp(cameraFollowPosition.z,-23,23));
+        cameraFollow.transform.position = cameraFollowPosition;
+        Debug.Log(cameraFollowPosition);
         
     }
     private Vector3 CameraClamped(Vector3 newPosition){
